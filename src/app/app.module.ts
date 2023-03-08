33a9ac59b48e1eaae5b77b4d94605fba20c72d5a
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2023-03-08 23:54:15 +0200

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {PostComponent} from "./post/post.component";
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import {TodoHeaderComponent} from "./todo-header/todo-header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    TodoHeaderComponent,
+    TodoListComponent,
+    ListItemComponent,
   ],
   imports: [
     BrowserModule
