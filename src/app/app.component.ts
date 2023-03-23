e1bd00d307e34c11620122c7aa1bacdb83544d24
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2023-03-23 14:35:18 +0200

    Number pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  e:number= Math.E
+  constructor() {
+    console.log(this.e)
+  }
 }
