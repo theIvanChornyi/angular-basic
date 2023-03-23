6ba5474ec92254c4a48ba4e6a91be59d637baee2
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2023-03-23 15:40:23 +0200

    Date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  e:number= Math.E
-  constructor() {
-    console.log(this.e)
-  }
+  e = Math.E;
+  string = 'Hello angular world';
+  date = new Date();
 }
