75a46f8214c83c7cb46102ac86e10e445a8d0360
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2023-04-05 12:46:47 +0300

    static modal created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { ModalComponent } from './components/modal/modal.component';
+import { BackdropComponent } from './components/backdrop/backdrop.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent],
+  declarations: [AppComponent, HomePageComponent, ModalComponent, BackdropComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
