f952d943c864502cd3922163e4ddd8ac03a07cab
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2023-04-10 14:52:04 +0300

    add service worker, create pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { ModalComponent } from './components/modal/modal.component';
 import { BackdropComponent } from './components/backdrop/backdrop.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,12 @@ import { BackdropComponent } from './components/backdrop/backdrop.component';
     ModalComponent,
     BackdropComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, SharedModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
